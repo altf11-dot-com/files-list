b28356ec0f30c380731a43fcd8bb9131dc3b3866
Jeff Brown <jeff@jeffbrown.us>
2020-01-04 00:00:49 -0500

    03-Jan-2020

---
+++ jbus-dir.bat
@@ -0,0 +1,18 @@
+@ECHO OFF
+IF [%1]==[] GOTO USAGE
+IF [%1]==[PDF] GOTO DIR_BY_TYPE
+IF [%1]==[PNG] GOTO DIR_BY_TYPE
+IF [%1]==[JPG] GOTO DIR_BY_TYPE
+IF [%1]==[XLS] GOTO DIR_BY_TYPE
+IF [%1]==[DOC] GOTO DIR_BY_TYPE
+:DIR_USER_CUSTOM
+DIR %1 > files-list.txt
+GOTO END_OF_BAT
+:DIR_BY_TYPE
+DIR *.%1? /S
+GOTO END_OF_BAT
+:USAGE
+ECHO Usage:
+ECHO "jbus-dir <PDF|PNG|JPG|XLS|DOC> (or enter your own custom expression)"
+:END_OF_BAT
+ECHO ON
